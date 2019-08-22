3c835f0bdcbd62d52ae8752568f420e36c2d9539
Kaylah Gore <43583709+kylhgr@users.noreply.github.com>
2019-08-21 21:23:15 -0400

    Add files via upload

---
+++ fizzbuzz.go
@@ -0,0 +1,28 @@
+// coding problem: print number 0 - 100
+// if number is divisile by 3, print fizz
+// if number is divisible by 5, print buzz
+// if numbrt is divisible by 3 and 5, print fizz buzz
+
+package main
+
+import "fmt"
+
+func fizzbuzz() {
+	countTo := 100
+
+	for i := 0; i <= countTo; i++ {
+		if i%15 == 0 {
+			fmt.Println("Fizz Buzz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
+
+func main() {
+	fizzbuzz()
+}
